68802cb98549cb0a65f1d47b984fef4498a528dd
Anderson Banihirwe <axbanihirwe@ualr.edu>
2016-09-03 15:28:36 -0500

    read Whole file

--- Python-for-Everybody-Specialization/Python-Data-Structures/readandopen.py
+++ Python-for-Everybody-Specialization/Python-Data-Structures/readandopen.py
@@ -1,7 +1,14 @@
-fhand = open('loremIpsum.txt')
+fhand = open('loremIpsum.txt','r')
 count = 0
 
 for line in fhand:
 	count += 1
 
 print 'Line Count:', count 
+
+# Save the read whole file in a string
+
+for line in iter(fhand):
+	print line 
+
+fhand.close()
