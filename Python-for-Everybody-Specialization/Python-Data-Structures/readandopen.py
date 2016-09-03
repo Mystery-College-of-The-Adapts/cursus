64fb9c0225f6ba0a92561674cc4910b1669042b9
Anderson Banihirwe <axbanihirwe@ualr.edu>
2016-09-03 15:42:10 -0500

    Skipping with Continue, Error Handling

--- Python-for-Everybody-Specialization/Python-Data-Structures/readandopen.py
+++ Python-for-Everybody-Specialization/Python-Data-Structures/readandopen.py
@@ -15,5 +15,31 @@ for line in iter(a):
 print "Searching Through a File:"
 f = open('loremIpsum.txt')
 for line in f:
+	line = line.rstrip()
 	if line.startswith('L'):
 		print line 
+
+# Skipping with Continue
+print "Skipping With Continue:\n"
+f = open('loremIpsum.txt')
+for line in f:
+	line = line.rstrip()
+	if line.startswith('L'):
+		continue
+
+	print line 
+
+
+fname = raw_input('Enter the file name:')
+try:
+	fhand = open(fname)
+
+except:
+	print 'File cannot be opened:', fname
+	exit()
+
+count = 0
+for line in fhand:
+	if line.startswith('Le'):
+		count += 1
+print "There were", count, 'Le lines in ', fname
