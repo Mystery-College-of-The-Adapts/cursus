18e5257fc672014e7beea332ca2c9cd93dce57c6
Anderson Banihirwe <axbanihirwe@ualr.edu>
2016-10-11 17:24:43 -0500

    Integer Addition

---
+++ Digital Design and Computer Architecture/MIPS Assembly Programming/adding_integers.asm
@@ -0,0 +1,15 @@
+.data
+	num1 : .word 5
+	num2:  .word 6
+
+.text
+	lw $t0, num1($zero)
+	lw $t1, num2($zero)
+	
+	add $t2, $t0, $t1    # t2 = t0 + t1
+	
+	li $v0, 1
+	add $a0, $zero, $t2
+	syscall 
+	
+	
