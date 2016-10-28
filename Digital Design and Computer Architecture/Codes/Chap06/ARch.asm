4262b0e3bd8553a127c025ee5b1f8ab5d54c86ea
Anderson Banihirwe <axbanihirwe@ualr.edu>
2016-10-27 19:25:28 -0500

    :tada: Accessing Byte-Addressable Memory

--- Digital Design and Computer Architecture/Codes/Chap06/ARch.asm
+++ Digital Design and Computer Architecture/Codes/Chap06/ARch.asm
@@ -21,3 +21,14 @@ lw $s3, 1($0)    # read memory word 1 into $3
 
 sw $s7, 5($0)    # Write $s7 to memory word 5 
 
+
+# Accessing Byte-Addressable Memory
+
+lw $s0, 0($0)       # read data word 0 (0xABCDEF78) into $s0
+lw $s1, 8($0)       # read data word 2 (0x01EE2842) into $s1 
+lw $s2, 0xC($0)     # read data word 3 (0x40F30788) into $s2
+
+sw $s3, 4($0)       # write $s3 to data word 1
+sw $s4, 0x20($0)    # write $s4 to data word 8
+sw $s5, 400($0)     # write $s5 to data word 100
+
