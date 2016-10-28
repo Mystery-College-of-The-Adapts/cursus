ef35fb93b0072850f7a833cc72060ea8e5db1a95
Anderson Banihirwe <axbanihirwe@ualr.edu>
2016-10-27 19:17:17 -0500

    :tada: Reading and Writing Word-Addressable Memory

--- Digital Design and Computer Architecture/Codes/Chap06/ARch.asm
+++ Digital Design and Computer Architecture/Codes/Chap06/ARch.asm
@@ -9,3 +9,15 @@ add $s0, $s1, $s2            # a = b + c
 
 sub $t0, $s2, $s3       # t =  c - d
 add $s0, $s1, $t0       # a = b + t 
+
+
+
+# Reading Word-Addressable Memory
+
+lw $s3, 1($0)    # read memory word 1 into $3
+
+
+# Writing Word-Addressable Memory
+
+sw $s7, 5($0)    # Write $s7 to memory word 5 
+
